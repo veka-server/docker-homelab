5ceae2fed9063bc0ea307191a25c154f168bcc04
Veka <veka610@gmail.com>
2025-01-05 15:43:15 +0100

    Create reset_docker_container.sh

---
+++ reset_docker_container.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+if [ $(id -u) -ne 0 ]
+  then echo Please run this script as root or using sudo!
+  exit
+fi
+
+# stop tout les container actuel
+docker stop $(docker ps -a -q); 
+
+# supprime tout les container
+docker rm $(docker ps -a -q); 
+
+# supprime tout les volumes
+docker volume prune; 
+
+# telecharge le docker-compose.yml
+
+# execute le docker-compose
+docker-compose up -d;
+
+# ajoute un model a ollama
+docker exec -ti ollama ollama run smallthinker ;
+
+
